8514439cff4e63452e97c61dceebdfe650cae07a
Arjun Ravikumar <54692274+arjunravikumar@users.noreply.github.com>
2021-04-09 20:32:52 -0400

    Add files via upload

---
+++ videobuffer.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import youtube_dl
+import boto3
+import math
+from time import sleep
+
+def showBoundingBoxPositionsForEachPerson(imageHeight, imageWidth, box, img): 
+    left = imageWidth * box['Left']
+    top = imageHeight * box['Top']
+    start_point = (int(left), int(top))
+    end_point = (math.ceil(left + (imageWidth*box['Width'])), math.ceil(top + (imageHeight*box['Height'])))
+    color = (0, 0, 0)
+    thickness = 2
+    img = cv2.rectangle(img,start_point, end_point,color,thickness)
+    return img
+
+def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img, confidence ,maskStatus):
+    left = imageWidth * box['Left']
+    top = imageHeight * box['Top']
+    start_point = (int(left), int(top))
+    end_point = (math.ceil(left + (imageWidth*box['Width'])), math.ceil(top + (imageHeight*box['Height'])))
+    if(maskStatus == "true"):
+        color = (0, 255, 0)
+    else:
+        color = (0, 0, 255)
+    thickness = 2
+    img = cv2.rectangle(img,start_point, end_point,color,thickness)
+    img = cv2.putText(img, "Confidence :"+ str(confidence), start_point, cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, color, thickness, cv2.LINE_AA)
+    return img
+
+def extractFaceDetails(bodyPart):
+    confidence = 0.0
+    maskStatus = False
+    box = None
+    if( "EquipmentDetections" in bodyPart and "BoundingBox" in bodyPart["EquipmentDetections"]):
+        box = bodyPart["EquipmentDetections"]["BoundingBox"]
+        if( CoversBodyPart in bodyPart["EquipmentDetections"] and "Confidence" in bodyPart["EquipmentDetections"]["CoversBodyPart"]):
+            confidence = bodyPart["EquipmentDetections"]["CoversBodyPart"]["Confidence"]
+            maskStatus = bodyPart["EquipmentDetections"]["CoversBodyPart"]["Value"]
+    return box,confidence,maskStatus
+
+def putImageInBucket():
+    s3Bucket = boto3.client('s3', region_name='us-east-1')
+    s3Bucket.upload_file("images/peopleWithBoundingBoxed.jpg", "myprojecttestbucket", "peopleWithBoundingBoxed.jpg")
+
+def captureImage():
+    video_url = 'https://www.youtube.com/watch?v=oIBERbq2tLA'
+
+    ydl_opts = {}
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+    info_dict = ydl.extract_info(video_url, download=False)
+    formats = info_dict.get('formats',None)
+    for f in formats:
+        if(f["height"] == 720):
+            url = f['url']
+            cap = cv2.VideoCapture(url)
+            ret, videoFrame = cap.read()
+            frame = videoFrame.copy()
+            if ret:
+                hasFrame, imageBytes = cv2.imencode(".jpg", frame)
+                if hasFrame:
+                    session = boto3.session.Session()
+                    rekognition = session.client('rekognition', region_name='us-east-1')
+                    response = rekognition. detect_protective_equipment(
+                            Image={
+                                'Bytes': imageBytes.tobytes(),
+                            }
+                        )
+                    for i in range(len(response['Persons'])):
+                        person = response['Persons'][i]
+                        h, w, c = frame.shape
+                        frame = showBoundingBoxPositionsForEachPerson(h,w,person["BoundingBox"],frame)
+                        for i in range(len(person["BodyParts"])):
+                            bodyPart = person["BodyParts"][i]
+                            if("Name" in bodyPart and bodyPart["Name"] == "FACE"):
+                                faceBoxDetails,faceCoverConfidence,maskStatus = extractFaceDetails(bodyPart)
+                                print(faceBoxDetails,faceCoverConfidence,maskStatus)
+                                if(faceBoxDetails!= None):
+                                    frame = showBoundingBoxPositionForFace(h,w,faceBoxDetails,frame,faceCoverConfidence,maskStatus)
+                    cv2.imwrite("images/peopleWithBoundingBoxed.jpg", frame)
+                    putImageInBucket()
+            cap.release()
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    while(True):
+        captureImage()
+        sleep(1)
