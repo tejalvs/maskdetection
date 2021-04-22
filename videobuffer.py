9028a6b385985a1bfba26099e6519c2bd7db8b80
Arjun Ravikumar <54692274+arjunravikumar@users.noreply.github.com>
2021-04-22 18:31:12 -0400

    Update videobuffer.py

--- videobuffer.py
+++ videobuffer.py
@@ -5,27 +5,25 @@ import boto3
 import math
 from time import sleep
 
-def showBoundingBoxPositionsForEachPerson(imageHeight, imageWidth, box, img): 
+def showBoundingBoxPositionsForEachPerson(imageHeight, imageWidth, box, img, maskStatus, confidence): 
     left = imageWidth * box['Left']
     top = imageHeight * box['Top']
     start_point = (int(left), int(top))
     end_point = (math.ceil(left + (imageWidth*box['Width'])), math.ceil(top + (imageHeight*box['Height'])))
-    color = (0, 0, 0)
+    if(maskStatus == "true"):
+        color = (0, 0, 255)
+    elif(maskStatus == "false"):
+        color = (0, 255, 0)
+    else:
+        color = (255, 0, 0)
     thickness = 2
     img = cv2.rectangle(img,start_point, end_point,color,thickness)
+    textLocation = (math.ceil(left + (imageWidth*box['Width'])), int(top))
+    if(confidence>1):
+        img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%", textLocation, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0), 1, cv2.LINE_AA)
     return img
 
-def showBoundingBoxPositionsForHead(imageHeight, imageWidth, box, img): 
-    left = imageWidth * box['Left']
-    top = imageHeight * box['Top']
-    start_point = (int(left), int(top))
-    end_point = (math.ceil(left + (imageWidth*box['Width'])), math.ceil(top + (imageHeight*box['Height'])))
-    color = (255, 0, 0)
-    thickness = 2
-    img = cv2.rectangle(img,start_point, end_point,color,thickness)
-    return img
-
-def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img, confidence ,maskStatus):
+def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img ,maskStatus):
     left = imageWidth * box['Left']
     top = imageHeight * box['Top']
     start_point = (int(left), int(top))
@@ -36,9 +34,7 @@ def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img, confidence
         color = (0, 255, 0)
     print(maskStatus,color)
     thickness = 1
-    textLocation = (math.ceil(left + (imageWidth*box['Width'])), int(top))
     img = cv2.rectangle(img,start_point, end_point,color,thickness)
-    img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%", start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0), thickness, cv2.LINE_AA)
     return img
 
 def extractFaceDetails(bodyPart):
@@ -87,18 +83,16 @@ def captureImage():
                     for i in range(len(response['Persons'])):
                         person = response['Persons'][i]
                         h, w, c = frame.shape
-                        frame = showBoundingBoxPositionsForEachPerson(h,w,person["BoundingBox"],frame)
+                        maskStatus="Not Sure"
+                        faceCoverConfidence = 0.0
                         for i in range(len(person["BodyParts"])):
                             bodyPart = person["BodyParts"][i]
                             if("Name" in bodyPart and bodyPart["Name"] == "FACE"):
                                 faceBoxDetails,faceCoverConfidence,maskStatus = extractFaceDetails(bodyPart)
                                 print("maskworn? ",maskStatus,faceBoxDetails)
                                 if(faceBoxDetails!= None):
-                                    frame = showBoundingBoxPositionForFace(h,w,faceBoxDetails,frame,faceCoverConfidence,maskStatus)
-                            elif("Name" in bodyPart and bodyPart["Name"] == "HEAD"):
-                                headBoxDetails,headCoverConfidence,headStatus = extractFaceDetails(bodyPart)
-                                if(headBoxDetails!= None):
-                                    frame = showBoundingBoxPositionsForHead(h,w,headBoxDetails,frame)
+                                    frame = showBoundingBoxPositionForFace(h,w,faceBoxDetails,frame,maskStatus)
+                        frame = showBoundingBoxPositionsForEachPerson(h,w,person["BoundingBox"],frame,maskStatus,faceCoverConfidence)
                     cv2.imwrite("peopleWithBoundingBoxed.jpg", frame)
                     putImageInBucket()
             cap.release()
