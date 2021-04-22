c44e88222d3fdd7dd5a2727b07c2ea7598ef4aa7
Arjun Ravikumar <54692274+arjunravikumar@users.noreply.github.com>
2021-04-22 17:21:03 -0400

    Update videobuffer.py

--- videobuffer.py
+++ videobuffer.py
@@ -27,7 +27,7 @@ def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img, confidence
         color = (0, 0, 255)
     thickness = 2
     img = cv2.rectangle(img,start_point, end_point,color,thickness)
-    img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%, start_point, cv2.FONT_HERSHEY_SIMPLEX, 
+    img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%", start_point, cv2.FONT_HERSHEY_SIMPLEX, 
                    0.2, color, thickness, cv2.LINE_AA)
     return img
 
