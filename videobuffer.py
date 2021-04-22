e103d07c963035dd74bfc5d54a97310a8261071e
Arjun Ravikumar <54692274+arjunravikumar@users.noreply.github.com>
2021-04-22 18:44:22 -0400

    Update videobuffer.py

--- videobuffer.py
+++ videobuffer.py
@@ -20,7 +20,7 @@ def showBoundingBoxPositionsForEachPerson(imageHeight, imageWidth, box, img, mas
     img = cv2.rectangle(img,start_point, end_point,color,thickness)
     textLocation = (math.ceil(left + (imageWidth*box['Width'])), int(top))
     if(int(confidence)>1):
-        img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%", textLocation, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0), 1, cv2.LINE_AA)
+        img = cv2.putText(img, "Confidence :"+ str(round(confidence,1))+"%", textLocation, cv2.FONT_HERSHEY_SIMPLEX, 0.3, color, 1, cv2.LINE_AA)
     return img
 
 def showBoundingBoxPositionForFace(imageHeight, imageWidth, box, img ,maskStatus):
